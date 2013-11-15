14ea762f3ae20d8faf70f23a649bed4a5972482b
Scott Opell <me@scottopell.com>
2013-11-15 10:11:38 -0500

    stripped some html

--- my_new_ruby.rb
+++ my_new_ruby.rb
@@ -3,4 +3,8 @@ require 'net/http'
 puts "what should I google?"
 query = gets.chomp
 uri = URI.parse("http://www.google.com/search?q=#{query.gsub(" ", "+")}&btnI")
-puts Net::HTTP.get(uri)
+content =  Net::HTTP.get(uri)
+
+content.gsub!("<", "")
+puts content.gsub!(">", "")
+
